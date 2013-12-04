d98e10c904ec109da31e4ebb1fbd6758761dd8da
Louisa Barrett <louisabarrett@gmail.com>
2013-12-04 14:39:08 -0700

    add homepage link to header

---
+++ test/features/return_to_homepage_via_main_logo_test.rb
@@ -0,0 +1,15 @@
+require "./test/test_helper"
+
+class ReturnToHomepageViaMainLogoTest < Capybara::Rails::TestCase
+
+  test "homepage can be returned to from a restaurant page" do
+    restaurant = FactoryGirl.create(:restaurant)
+    visit restaurant_path(restaurant)
+    within("#main_logo") do
+      save_and_open_page
+      click_on "retto"
+    end
+    assert current_path == root_path
+  end
+
+end
