dfd420b071a4fa6f7ac037258bb10f1f594d6fe8
Will Mitchell <wvmitchell@gmail.com>
2013-12-09 16:09:47 -0700

    User can now create account, email settings need to be fixed

--- test/features/create_user_test.rb
+++ test/features/create_user_test.rb
@@ -8,12 +8,13 @@ class CreateUserTest < Capybara::Rails::TestCase
     assert_content page, "Become a New User"
   end
 
-  test "a user is created" do 
+  test "a user is created" do
     visit new_user_path
     fill_in "Username", with: "Bob"
     fill_in "Email", with: "bob@example.com"
     fill_in "Password", with: "password"
     click_on "Create my account"
+    save_and_open_page
     assert_content page, "User Bob created!"
   end
 
