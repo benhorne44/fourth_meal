18d5bb039dcb4aa39ea2ce7638fa78ff7b271ac4
Will Mitchell <wvmitchell@gmail.com>
2013-12-10 20:38:44 -0700

    Changed restaurant factory to make restaurants which are published and active

--- test/factories/restaurants.rb
+++ test/factories/restaurants.rb
@@ -5,5 +5,11 @@ FactoryGirl.define do
     name "Will's Waffles"
     location "123 Denver Street"
     phone_number "123-456-7890"
+
+    after(:create) do |restaurant|
+      restaurant.published = true
+      restaurant.active = true
+      restaurant.save
+    end
   end
 end
